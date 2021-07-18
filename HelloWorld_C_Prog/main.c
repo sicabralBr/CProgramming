b496a43c532d75f8ac36acdb679fdc9f54d2281e
Simone Cabral <simonecabral.br@gmail.com>
2021-07-17 23:22:19 -0300

    Adding new C content

---
+++ HelloWorld_C_Prog/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello World\n");
+    printf("%s %s\n", "Hello", "World");
+
+    char c = getchar(); //waits for the user to press enter before exiting
+}
